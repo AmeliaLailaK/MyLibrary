338e34cddd5601c6d71895ced61a21760e6d81fa
Amelia Laila K <amelia@qburst.com>
2023-12-27 08:38:58 +0530

    First commit

---
+++ Sources/MyLibrary/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  FrameworkDist
+//
+//  Created by amelia laila on 27/12/23.
+//
+
+import Foundation
+
+
+struct SDK{
+    static func doSomeWork(){
+        print("Doing some work")
+    }
+}
